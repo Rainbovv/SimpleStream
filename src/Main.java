ecfe91ac49caf09c2d3b75f05b842e7d1ac38963
Rainbovv <turcanu.radu17@gmail.com>
2020-05-18 20:37:02 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,37 @@
-import org.w3c.dom.ls.LSOutput;
-
+import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        final int VALID_PIN  = 1234;
-        int scannedPin = 0;
-        String pin = "";
+        String dish;
+        double price;
+        byte quantity;
+        double finalPrice = 0;
 
-        System.out.println("PLEASE, ENTER YOUR PIN");
-        pin += (char)System.in.read();
-        pin += (char)System.in.read();
-        pin += (char)System.in.read();
-        pin += (char)System.in.read();
-        scannedPin = Integer.parseInt(pin);
+        File file = new File("src/orders.txt");
 
-        System.out.println(pin);
-        System.out.println("Account accessible? " + (VALID_PIN == scannedPin));
+        if (!file.exists()) System.err.println("FILE DOES NOT EXIST!");
+        else {
+            Scanner in = new Scanner(file);
 
+            System.out.println("\n-------------------------\nYou ordered:\n");
 
+            for (int i = 0; i < 3; i++) {
+                dish = in.next();
+                price = in.nextDouble();
+                quantity = in.nextByte();
+                finalPrice += price * quantity;
+                System.out.printf("%-15s %d x %5.2f%n", dish, quantity, price);
+            }
+            System.out.printf("%nFinal price %13.2f%n-------------------------%n", finalPrice);
+        }
     }
 }
+//    Написать код в main() который проверит если данный файл доступен (метод .exists() - класс File)
+//        - и если нет - выдаст ошибку на экране!
+//
+//        При помощи цикла for который повториться ровно 3 раза (у нас в файле 3 строки), прочитает
+//        значения в виде 3-х переменных и выведет на экран список вот этих заказанных блюд.
+//
+//        При выводе внутри цикла, вычислит так же общую стоимость и распечатает в конце в 4-й строке
