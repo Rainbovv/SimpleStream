c526be9f1d6eb750c00c3c72c3602cd77ea84137
Rainbovv <turcanu.radu17@gmail.com>
2020-05-20 12:27:25 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,30 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String dish;
         double price;
         byte quantity;
         double finalPrice = 0;
 
-        File file = new File("src/orders.txt");
+        try {
+            FileWriter fw = new FileWriter("orders.txt");
+            fw.write("Pizza 75,00 2\nKebab 45,00 1\nCola 9,50 3");
+            fw.close();
+        } catch (IOException e) {
+            System.err.println("CANNOT CREATE A FILE!");
+        }
 
-        if (!file.exists()) System.err.println("FILE DOES NOT EXIST!");
-        else {
+        try {
+            File file = new File("orders.txt");
             Scanner in = new Scanner(file);
 
             System.out.println("\n-------------------------\nYou ordered:\n");
-
             for (int i = 0; i < 3; i++) {
                 dish = in.next();
                 price = in.nextDouble();
@@ -25,13 +33,12 @@ public class Main {
                 System.out.printf("%-15s %d x %5.2f%n", dish, quantity, price);
             }
             System.out.printf("%nFinal price %13.2f%n-------------------------%n", finalPrice);
+
+        } catch (FileNotFoundException e) {
+            System.err.println("FILE DOES NOT EXIST!");
+
+        } catch (InputMismatchException e) {
+            System.err.println("WRONG FILE FORMAT!");
         }
     }
 }
-//    Написать код в main() который проверит если данный файл доступен (метод .exists() - класс File)
-//        - и если нет - выдаст ошибку на экране!
-//
-//        При помощи цикла for который повториться ровно 3 раза (у нас в файле 3 строки), прочитает
-//        значения в виде 3-х переменных и выведет на экран список вот этих заказанных блюд.
-//
-//        При выводе внутри цикла, вычислит так же общую стоимость и распечатает в конце в 4-й строке
